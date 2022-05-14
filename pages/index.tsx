8577a10e555a5039bbe5af3739dd6db017b68770
Himanshu Dhiman <45459993+himdhiman@users.noreply.github.com>
2022-05-14 11:56:48 +0530

    terminal removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import Hero from 'components/Hero'
-import Feature from 'components/Feature/index'
-import Head from 'next/head'
-import Navbar from 'components/NavbarT'
-import Terminal from 'components/Terminal'
+import React from "react";
+import Hero from "components/Hero";
+import Feature from "components/Feature/index";
+import Head from "next/head";
+import Navbar from "components/NavbarT";
+// import Terminal from 'components/Terminal'
 
 function Homepage() {
   return (
-    <div className='backgroundGrdient'>
-      <Navbar/>
-      <Head><title>DirtyBits</title></Head>
+    <div className="backgroundGrdient">
+      <Navbar />
+      <Head>
+        <title>DirtyBits</title>
+      </Head>
       <Hero />
-      <Terminal/>
-      <Feature title="Features" description="Some of our basics features"/>
+      {/* <Terminal/> */}
+      <Feature title="Features" description="Some of our basics features" />
     </div>
-  )
+  );
 }
 
 export default Homepage;
